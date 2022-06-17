abd34225af111fdb4897122076f1c6397b57936f
Lizzy <qschukkink@gmail.com>
2022-06-17 21:23:18 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,12 +24,8 @@ intersphinx_mapping = {
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
-
 templates_path = ['_templates']
-
-# -- Options for HTML output
-
 html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
 epub_show_urls = 'footnote'
+source_suffix = '.rst'
+master_doc = 'index'
